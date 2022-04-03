1302444e65d6baf44fd434e822ef20d7faa527de
BinniZenobioCordovaLeandro <binni.2000.cordova@gmail.com>
2022-04-03 14:47:17 -0500

    added anounce test mode

--- lib/src/features/user_feature/logic/sign_in_controller.dart
+++ lib/src/features/user_feature/logic/sign_in_controller.dart
@@ -6,12 +6,16 @@ import 'package:pickpointer/packages/session_package/domain/entities/abstract_se
 import 'package:pickpointer/packages/session_package/domain/usecases/update_session_usecase.dart';
 import 'package:pickpointer/packages/session_package/domain/usecases/verify_session_usecase.dart';
 import 'package:pickpointer/src/core/providers/google_sign_in_provider.dart';
+import 'package:pickpointer/src/core/providers/notification_provider.dart';
 
 class SignInController extends GetxController {
   static SignInController get instance => Get.put(SignInController());
 
   final GoogleSignInProvider? _googleSignInProvider =
       GoogleSignInProvider.getInstance();
+
+  final NotificationProvider? notificationProvider =
+    NotificationProvider.getInstance();
 
   var isSigned = false.obs;
   var googleIsLoading = false.obs;
@@ -23,6 +27,17 @@ class SignInController extends GetxController {
   final UpdateSessionUsecase _updateSessionUsecase = UpdateSessionUsecase(
     abstractSessionRepository: SharedPreferencesFirebaseSessionDatasources(),
   );
+
+  Future<bool>? sendNotification() {
+    Future<bool>? futureBool = notificationProvider
+        ?.sendNotification(
+          title: 'BIENVENIDO A PICKPOINTER (MODO TEST)',
+          body:
+              'Esta app permite viajar rapido y barato.\nDisfrute el modo TEST. Pronto saldremos a PRODUCCIÓN!.',
+        )
+        .then((value) => value);
+    return futureBool;
+  }
 
   Future<bool> signIn(number) async {
     if (number == 0) {
@@ -45,6 +60,7 @@ class SignInController extends GetxController {
           googleIsLoading.value = false;
           isSigned.value = true;
         });
+        sendNotification();
         return true;
       } else {
         googleIsLoading.value = false;

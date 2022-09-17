3595dd232481901fc07ce73cda6f7f4ddc1fc3f9
BinniZenobioCordovaLeandro <binni.2000.cordova@gmail.com>
2022-09-17 18:21:04 -0500

    Added hook to newRoute approved
    send notification to topic app when new routes is approved

--- functions/src/triggers/index.ts
+++ functions/src/triggers/index.ts
@@ -2,6 +2,7 @@ import * as functions from "firebase-functions";
 import {Change, EventContext} from "firebase-functions/v1";
 import {QueryDocumentSnapshot} from "firebase-functions/v1/firestore";
 import * as users from "./users";
+import * as routesRequests from "./routesRequests";
 
 export const handler = (change: Change<QueryDocumentSnapshot>, context: EventContext) => {
   const collectionName = context.params.collection;
@@ -9,6 +10,10 @@ export const handler = (change: Change<QueryDocumentSnapshot>, context: EventCon
   switch (collectionName) {
     case "c_users":
       users.onUpdate(change);
+      break;
+
+    case "c_routes_requests":
+      routesRequests.onUpdate(change);
       break;
 
     default:
